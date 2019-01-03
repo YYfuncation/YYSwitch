bc933b5ef165dad6d3cc55f014c42cf11054be3f
Liandi <56850682@qq.com>
2019-01-03 09:42:14 +0800

    Initial Commit

---
+++ YYSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYSwitch
+//
+//  Created by Liandi on 2019/1/3.
+//  Copyright © 2019年 Liandi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YYSwitch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YYSwitch
+//
+//  Created by Liandi on 2019/1/3.
+//  Copyright © 2019年 Liandi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
